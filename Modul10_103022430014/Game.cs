305bb82d0b37ad534e4c08b5a4cafe5c95458ea1
Bangbyan <mhmdabyan19@gmail.com>
2026-05-09 15:12:57 +0700

    Menambahkan API Game

---
+++ Modul10_103022430014/Game.cs
@@ -0,0 +1,34 @@
+﻿namespace Modul10_103022430014
+{
+    public class Game
+    {
+        public int id { get; set; }
+        public string Nama { get; set; }
+        public string Developer { get; set; }
+        public int TahunRilis { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+        public string[] Platform { get; set; }
+        public string[] Mode { get; set; }
+        public bool IsOnline { get; set; }
+        public int Harga { get; set; }
+        public Game()
+        {
+
+        }
+
+        public Game (int id, string nama, string developer, int tahunRilis, string genre, double rating, string[] platform, string[] mode, bool isOnline, int harga)
+        {
+            this.id = id;
+            Nama = nama;
+            Developer = developer;
+            TahunRilis = tahunRilis;
+            Genre = genre;
+            Rating = rating;
+            Platform = platform;
+            Mode = mode;
+            IsOnline = isOnline;
+            Harga = harga;
+        }
+    }
+}
---
+++ Modul10_103022430014/GameController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+namespace Modul10_103022430014
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GameController : ControllerBase
+    {
+        private static List<Game> dataGame = new List<Game>
+        {
+            new Game { id = 1, Nama = "Valorant", Developer = "Riot Games", TahunRilis = 2020, Genre = "FPS", Rating = 8.5, Platform =  ["PC"], Mode = ["Multiplayer"], IsOnline = true, Harga = 0},
+            new Game { id = 2, Nama = "GTA V", Developer =  "Rockstar Games", TahunRilis = 2013, Genre = "Open World", Rating =  9.5, Platform =  ["PC”, “PS4”, “PS5”, “Xbox"], Mode = ["Singleplayer”,“Multiplayer"], IsOnline = true, 
+                Harga = 300000},
+            new Game { id = 3, Nama = "The Witcher 3", Developer = "CD Projekt Red", TahunRilis = 2015, Genre = "RPG", Rating = 9.7, Platform =  ["PC”, “PS4”, “PS5”, “Xbox”, “Switch"], 
+                Mode =  ["Singleplayer"], IsOnline =  false, Harga = 250000}
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Game>> Get()
+        {
+            return Ok(dataGame); 
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Game> Get(int id)
+        {
+            if (id < 0 || id >= dataGame.Count) return NotFound();
+            return dataGame[id];
+        }
+        [HttpPost]
+        public IActionResult Post(Game newGame)
+        {
+            dataGame.Add(newGame);
+            return Ok(newGame);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (id < 0 || id >= dataGame.Count) return NotFound();
+            dataGame.RemoveAt(id);
+            return Ok();
+        }
+    }
+}
